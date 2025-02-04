bdef16df79976da6a87be2056ac61a4cc211811b
Majo-Alba <kangaroo.cacti@gmail.com>
2025-02-04 07:06:16 -0600

    Update quoterModel.js

--- server/models/quoterModel.js
+++ server/models/quoterModel.js
@@ -1,3 +1,5 @@
+// NEW COMMIT
+
 const { application } = require('express')
 const mongoose = require('mongoose')
 
@@ -52,6 +54,9 @@ const quoterSchema = mongoose.Schema({
     terminosYCondiciones: {
         type: String,
     },
+    receptorServicio:{
+        type: String,
+    },
     calleServicio: {
         type: String,
     },
@@ -102,3 +107,114 @@ const quoterSchema = mongoose.Schema({
 const quoterModel = mongoose.model("quoter_form", quoterSchema)
 
 module.exports = quoterModel
+
+// NEW COMMIT END
+
+// ORIGINAL COMMIT
+
+// const { application } = require('express')
+// const mongoose = require('mongoose')
+
+// // QUOTER SCHEMA SETUP
+// const quoterSchema = mongoose.Schema({
+//     empresa: {
+//         type: String,
+//     },
+//     contacto: {
+//         type: String,
+//     },
+//     calle: {
+//         type: String,
+//     },
+//     ciudadYEdo: {
+//         type: String,
+//     },
+//     cp: {
+//         type: String,
+//     },
+//     telPrincipal: {
+//         type: String,
+//     },
+//     correoPrincipal: {
+//         type: String,
+//     },
+//     rfcEmpresa: {
+//         type: String,
+//     },
+//     regimenEmpresa: {
+//         type: String,
+//     },
+//     correoFacturacion: {
+//         type: String,
+//     },
+//     tipoFactura: {
+//         type: String,
+//     },
+//     metodoPago: {
+//         type: String,
+//     },
+//     formaPago: {
+//         type: String,
+//     },
+//     plazosPago: {
+//         type: String,
+//     },
+//     descripcionProyecto: {
+//         type: String,
+//         maxLength: [1500]
+//     },
+//     terminosYCondiciones: {
+//         type: String,
+//     },
+//     calleServicio: {
+//         type: String,
+//     },
+//     numExteriorServicio: {
+//         type: String,
+//     },
+//     numInteriorServicio: {
+//         type: String,
+//     },
+//     colServicio: {
+//         type: String,
+//     },
+//     cpServicio: {
+//         type: String,
+//     },
+//     ciudadServicio: {
+//         type: String,
+//     },
+//     estadoServicio: {
+//         type: String,
+//     },
+//     fechaElaboracion: {
+//         type: String,
+//     },
+//     fechaValidez: {
+//         type: String,
+//     },
+//     idCotizacion: {
+//         type: String,
+//     },
+//     elaboradaPor: {
+//         type: String,
+//     },
+//     materials: {
+//         type: String,
+//     },
+//     labour: {
+//         type: String,
+//     },
+//     misc: {
+//         type: String,
+//     },
+//     descuento: {
+//         type: String,
+//     },
+// }, {timestamps: true})
+
+// const quoterModel = mongoose.model("quoter_form", quoterSchema)
+
+// module.exports = quoterModel
+
+// ORIGINAL COMMIT END
