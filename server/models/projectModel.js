c0943399db94c9719325210f0c07e941a67f9659
Majo-Alba <kangaroo.cacti@gmail.com>
2025-02-04 07:04:32 -0600

    Update projectModel.js

--- server/models/projectModel.js
+++ server/models/projectModel.js
@@ -1,3 +1,5 @@
+// NEW COMMIT
+
 const { application } = require('express')
 const mongoose = require('mongoose')
 
@@ -8,6 +10,11 @@ const projectSchema = mongoose.Schema({
         // type: mongoose.Schema.Types.ObjectId,
     // },
 
+    // NEW JUL22
+    empresa: {
+        type: String,
+    },
+    // END JUL22
     cliente: {
         type: String,
         required:[true, "Campo obligatorio"],
@@ -23,12 +30,25 @@ const projectSchema = mongoose.Schema({
     correoPrincipal: {
         type: String
     },
+    // NEW JUL22
+    numeroTelefono: {
+        type: String
+    },
+    // END JUL22
     direccion: {
         type: String
     },
     sucursal: {
         type: String
     },
+    // NEW JUL22
+    ubicacion: {
+        type: String
+    },
+    giro: {
+        type: String
+    },
+    // END JUL22
     correoFacturacion: {
         type: String
     },
@@ -70,10 +90,35 @@ const projectSchema = mongoose.Schema({
         minLength: [4, "Folio debe contener un mínimo de 4 caracteres"],
         maxLength: [50, "Folio no debe exceder los 50 caracteres"]
     },
-    file: {
+    // NEW JUL18
+    fileCIF: {
         type: String,
-        // type: Array
     },
+    fileCSF: {
+        type: String,
+    },
+    fileCartaConformidad: {
+        type: String,
+    },
+    fileCotizacion: {
+        type: String,
+    },
+    fileFactura: {
+        type: String,
+    },
+    fileOrdenCompra: {
+        type: String,
+    },
+    fileOtro: {
+        type: String,
+    },
+    // END JUL18
+    files: {
+        // type: String,
+        type: Array
+    },
+    // NEW OCT10
+    // END OCT10
     estatusProyecto: {
         type: String,
         //required:[true, "Campo obligatorio"],
@@ -130,9 +175,159 @@ const projectSchema = mongoose.Schema({
     extraPayment: {
         type: String,
     }, 
+    // NEW SEP23
+    item: {
+        type: Array
+    },
+    // END SEP23
   
 }, {timestamps: true})
 
 const projectModel = mongoose.model("project_forms", projectSchema)
 
 module.exports = projectModel
+
+// NEW COMMIT END
+
+// ORIGINAL COMMIT
+
+// const { application } = require('express')
+// const mongoose = require('mongoose')
+
+// //PROJECT SCHEMA SETUP
+// const projectSchema = mongoose.Schema({
+//     // _id: {
+//         // type: String,
+//         // type: mongoose.Schema.Types.ObjectId,
+//     // },
+
+//     cliente: {
+//         type: String,
+//         required:[true, "Campo obligatorio"],
+//         minLength: [4, "Cliente debe contener un mínimo de 4 caracteres"],
+//         maxLength: [100, "Cliente no debe exceder los 100 caracteres"]
+//     },
+//     numeroCliente: {
+//         type: String,
+//     },
+//     razonSocial: {
+//         type: String,
+//     },
+//     correoPrincipal: {
+//         type: String
+//     },
+//     direccion: {
+//         type: String
+//     },
+//     sucursal: {
+//         type: String
+//     },
+//     correoFacturacion: {
+//         type: String
+//     },
+//     regimenFiscal: {
+//         type: String
+//     },
+//     tipoFactura: {
+//         type: String
+//     },
+//     metodoPago: {
+//         type: String
+//     },
+//     formaPago: {
+//         type: String
+//     },
+//     usoCFDI: {
+//         type: String
+//     },
+//     idProyecto: {
+//         type: String,
+//         required:[true, "Campo obligatorio"],
+//         minLength: [4, "Nombre debe contener un mínimo de 4 caracteres"],
+//         maxLength: [100, "Nombre no debe exceder los 100 caracteres"]
+//     },
+//     inicioProceso: {
+//         type: String,
+//         //required:[true, "Campo obligatorio"]    
+//     },
+//     inicioTecnico: String,
+//     nombreEncargado: {
+//         type: String,
+//         //required:[true, "Campo obligatorio"],
+//         //minLength: [4, "Encargado debe contener un mínimo de 4 caracteres"],
+//         //maxLength: [50, "Encargado no debe exceder los 50 caracteres"]    
+//     },
+//     folioAceptado: {
+//         type: String,
+//         required:[true, "Campo obligatorio"],
+//         minLength: [4, "Folio debe contener un mínimo de 4 caracteres"],
+//         maxLength: [50, "Folio no debe exceder los 50 caracteres"]
+//     },
+//     file: {
+//         type: String,
+//         // type: Array
+//     },
+//     estatusProyecto: {
+//         type: String,
+//         //required:[true, "Campo obligatorio"],
+//         //minLength: [4, "Estatus debe contener un mínimo de 4 caracteres"],
+//         //maxLength: [100, "Estatus no debe exceder los 100 caracteres"],
+//     },
+//     subtotal: {
+//         type: String,
+//     },
+//     total: {
+//         type: String,
+//     },
+//     ivaGenerado: {
+//         type: String
+//     },
+//     // NEW JUN04
+//     anticipo: {
+//         type: String,
+//     },
+//     plazosDePago: {
+//         type: Number,
+//     },
+//     estatusContable: {
+//         type: String
+//     },
+//     // END JUN04
+//     // NEW JUN04.2
+//     pagado: {
+//         type: String,
+//     },
+//     porPagar: {
+//         type: String,
+//     },
+//     pagoAdicional: {
+//         type: String,
+//     },
+//     fechaPago: {
+//         type: String
+//     },
+//     bancoPago: {
+//         type: String
+//     },
+//     // END JUN04.2
+//     comentarios: {
+//         type: String,
+//         maxLength: [350, "Estatus no debe exceder los 350 caracteres"]
+//     },
+//     condicion: {
+//         type: String
+//     },
+//     payment: {
+//         type: String,
+//     },
+//     extraPayment: {
+//         type: String,
+//     }, 
+  
+// }, {timestamps: true})
+
+// const projectModel = mongoose.model("project_forms", projectSchema)
+
+// module.exports = projectModel
+
+// ORIGINAL COMMIT END
